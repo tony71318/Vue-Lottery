da37f40471cbd73252fda47c952ef23a6e5244d7
Lindsay <lindsaytseng777@gmail.com>
2018-06-11 17:25:59 +0800

    add demo contract

--- src/util/getContract.js
+++ src/util/getContract.js
@@ -1,6 +1,7 @@
 import Web3 from 'web3'
 // import {address, ABI} from './constants/casinoContract'
 import {address, ABI} from './constants/lottoContract'
+import {demo_address, demo_ABI} from './constants/demoLottoContract'
 
 let getContract = new Promise(function (resolve, reject) {
   let web3 = new Web3(window.web3.currentProvider)
